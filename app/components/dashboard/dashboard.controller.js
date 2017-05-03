b43b1a563a8a6ec831d482b337b4d143150097b5
Oleg Lukashev <incognitorus@gmail.com>
2017-05-04 00:24:28 +0400

    #2 change container for inputs + add email validation + add overflow scroll for popup + changes for dashboard reservations

--- app/components/dashboard/dashboard.controller.js
+++ app/components/dashboard/dashboard.controller.js
@@ -1,5 +1,7 @@
+import angular from 'angular';
+
 export default class DashboardCtrl {
-  constructor(Reservation, moment, AppConstants, JWT, $window, $mdSidenav, $rootScope, $scope, $modal) {
+  constructor(Reservation, Table, moment, AppConstants, JWT, filterFilter, $window, $mdSidenav, $rootScope, $scope, $modal) {
     'ngInject';
 
     this.moment          = moment;
@@ -8,15 +10,19 @@ export default class DashboardCtrl {
     this.today           = [];
     this.$mdSidenav      = $mdSidenav;
     this.$rootScope      = $rootScope;
+    this.filterFilter    = filterFilter;
     this.$scope          = $scope;
     this.$modal          = $modal;
+    this.$window         = $window;
 
     this.Reservation     = Reservation;
+    this.Table           = Table;
 
     $scope.$on('DashboardCtrl.reload_reservations', () => {
       this.loadReservations();
     });
 
+    this.loadTables();
     this.loadReservations();
   }
 
@@ -39,8 +45,46 @@ export default class DashboardCtrl {
     });
   }
 
+  getTableNumbersByTableIds(table_ids) {
+    let result = [];
+    let that = this;
+    
+    angular.forEach(table_ids, function(value) {
+      let table = that.filterFilter(that.tables, { id: value })[0];
+
+      if (table) {
+        this.push(table.table_number);
+      }
+    }, result);
+
+    return result;
+  }
+
   parsedDate(date) {
-    return this.moment(date).format('h:mm');
+    return this.moment(date).format('HH:mm');
+  }
+
+  loadReservations() {
+    this.Reservation
+      .getAll()
+        .then(
+          (reservations) => {
+            this.reservationsLoaded = true;
+            this.action_required    = reservations.action_required;
+            this.group_this_week    = reservations.group_this_week;
+            this.today              = reservations.today;
+          });
+  }
+
+  loadTables() {
+    this.tables = [];
+
+    this.Table
+      .getAll()
+        .then(
+          (result) => {
+            this.tables = result;
+          });
   }
 
   loadReservations() {
