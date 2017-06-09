a8c18512b394d4269e252e2f057a81844c1eddc8
safrio <safrio@gmail.com>
2017-06-09 14:03:37 +0500

    add icons for each zone
    fixed buttom padding for select icons

--- app/components/settings/tables/settings_tables.new_zone.controller.js
+++ app/components/settings/tables/settings_tables.new_zone.controller.js
@@ -1,19 +1,31 @@
 export default class SettingsTablesNewZoneCtrl {
-  constructor(User, Zone, zones, $modalInstance) {
+  constructor(User, Zone, zones, $scope, $modalInstance) {
     'ngInject';
 
     this.current_company = User.current_company;
 
     this.Zone          = Zone;
     this.zones         = zones;
-    this.default_zones = ['Bar', 'Restaurant', 'Terras', 'Zaal'];
+    this.$scope        = $scope;
+    this.default_zones = ['Bar', 'Meeting room', 'Lounge', 'Restaurant', 'Terras', 'Zaal'];
     this.icons_classes = {
-      'Bar'        : 'mdi-martini',
-      'Restaurant' : 'mdi-food-fork-drink',
-      'Terras'     : 'mdi-beach',
-      'Zaal'       : 'mdi-food-fork-drink'
+      'Bar'          : 'mdi-martini',
+      'Lounge'       : 'mdi-food',
+      'Meeting room' : 'mdi-google-circles-communities',
+      'Restaurant'   : 'mdi-silverware-variant',
+      'Terras'       : 'mdi-beach',
+      'Zaal'         : 'mdi-food-fork-drink'
     }
-    this.$modalInstance = $modalInstance;
+    this.empty_mdi_class = 'mdi-close';
+    this.$modalInstance  = $modalInstance;
+    this.item            = {};
+
+    let loadedZones = this.zonesHash();
+    if (loadedZones) this.icons_classes = $.extend(loadedZones, this.icons_classes)
+  }
+
+  changeClass(className) {
+    this.item.class = className;
   }
 
   submitForm() {
@@ -26,7 +38,7 @@ export default class SettingsTablesNewZoneCtrl {
           this.is_submitting = false;
           this.zones.push(zone);
           this.$modalInstance.dismiss('cancel');
-        }, 
+        },
         (error) => {
           this.errors = error.data.errors;
         });
@@ -34,5 +46,13 @@ export default class SettingsTablesNewZoneCtrl {
 
   zonesNames() {
     return this.zones.map((item) => item.name);
+  }
+
+  zonesHash() {
+    let zones = {};
+    this.zones.map((item) => {
+      zones[item.name] = (item.class ? item.class : this.icons_classes[item.name]) || this.empty_mdi_class;
+    });
+    return zones;
   }
 }
