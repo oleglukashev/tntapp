cb86ed658ca4c56c5f12686fd1a5eb58559b2308
safrio <safrio@gmail.com>
2017-10-04 07:54:10 +0500

    #108 pdf: eslint refactoring

--- app/shared/user_menu/user_menu.controller.js
+++ app/shared/user_menu/user_menu.controller.js
@@ -1,31 +1,34 @@
 import angular from 'angular';
 
 export default class UserMenuCtrl {
-  constructor(User, Reservation, CustomerNote, CustomerPreference, CustomerAllergies, Customer, moment, $rootScope, $mdSidenav, $modal) {
+  constructor(
+    User, Reservation, CustomerNote, CustomerPreference, CustomerAllergies, Customer, moment,
+    $rootScope, $mdSidenav, $modal,
+  ) {
     'ngInject';
 
     this.current_company_id = User.getCompanyId();
 
-    this.Reservation        = Reservation;
-    this.CustomerNote       = CustomerNote;
+    this.Reservation = Reservation;
+    this.CustomerNote = CustomerNote;
     this.CustomerPreference = CustomerPreference;
-    this.CustomerAllergies  = CustomerAllergies;
+    this.CustomerAllergies = CustomerAllergies;
 
-    this.moment     = moment;
-    this.Customer   = Customer;
+    this.moment = moment;
+    this.Customer = Customer;
     this.$rootScope = $rootScope;
     this.$mdSidenav = $mdSidenav;
-    this.$modal     = $modal;
+    this.$modal = $modal;
   }
 
-  loadPDF(reservation_id) {
+  loadPDF(reservationId) {
     this.Reservation
-      .getPDF(this.current_company_id, reservation_id)
+      .getPDF(this.current_company_id, reservationId)
       .then();
   }
 
-  openEditReservationModal(reservation, reservation_part) {
-    let modalInstance = this.$modal.open({
+  openEditReservationModal(reservation, reservationPart) {
+    const modalInstance = this.$modal.open({
       templateUrl: 'reservation_part.edit.view.html',
       controller: 'ReservationPartEditCtrl as reserv',
       size: 'md',
@@ -34,35 +37,35 @@ export default class UserMenuCtrl {
           return reservation;
         },
         reservationPart: () => {
-          return reservation_part;
+          return reservationPart;
         },
-      }
+      },
     });
 
-    modalInstance.result.then((selectedItem) => {
-      //success
-      this.loadCustomerReservations(reservation.customer.id, reservation_part.id);
+    modalInstance.result.then(() => {
+      // success
+      this.loadCustomerReservations(reservation.customer.id, reservationPart.id);
     }, () => {
       // fail
     });
   }
 
-  openEditModal(user_id) {
-    let modalInstance = this.$modal.open({
+  openEditModal() {
+    const modalInstance = this.$modal.open({
       templateUrl: 'user_menu.edit.view.html',
       controller: 'UserMenuEditCtrl as edit_user',
-      size: 'md'
+      size: 'md',
     });
 
-    modalInstance.result.then((selectedItem) => {
-      //success
+    modalInstance.result.then(() => {
+      // success
     }, () => {
       // fail
     });
   }
 
   toggleStar(customer) {
-    let data = {
+    const data = {
       first_name: customer.first_name,
       last_name: customer.last_name,
       primary_phone_number: customer.primary_phone_number,
@@ -75,57 +78,61 @@ export default class UserMenuCtrl {
       date_of_birth: customer.date_of_birth,
       gender: customer.gender,
       averageRating: customer.average_rating,
-      regular: !customer.regular
+      regular: !customer.regular,
     };
 
-    this.Customer.edit(this.current_company_id, customer.id, data).then(customer => {
-      this.$rootScope.customer = customer;
+    this.Customer.edit(this.current_company_id, customer.id, data).then((responseCustomer) => {
+      this.$rootScope.customer = responseCustomer;
     });
   }
 
-  openCustomerMenu(customer_id, reservation_part_id) {
-    this.CustomerPreference.getAll(this.current_company_id, customer_id)
-    .then(preferences => {
-      this.$rootScope.customer_preferences = preferences;
-    });
+  openCustomerMenu(customerId, reservationPartId) {
+    this.CustomerPreference.getAll(this.current_company_id, customerId)
+      .then((preferences) => {
+        this.$rootScope.customer_preferences = preferences;
+      });
 
-    this.CustomerNote.getAll(this.current_company_id, customer_id)
-    .then(notes => {
-      this.$rootScope.customer_notes = notes;
-    });
+    this.CustomerNote.getAll(this.current_company_id, customerId)
+      .then((notes) => {
+        this.$rootScope.customer_notes = notes;
+      });
 
-    this.CustomerAllergies.getAll(this.current_company_id, customer_id)
-    .then(allergies => {
-      this.$rootScope.customer_allergies = allergies;
-    });
+    this.CustomerAllergies.getAll(this.current_company_id, customerId)
+      .then((allergies) => {
+        this.$rootScope.customer_allergies = allergies;
+      });
 
-    this.Customer.findById(this.current_company_id, customer_id).then(customer => {
+    this.Customer.findById(this.current_company_id, customerId).then((customer) => {
       this.$rootScope.customer = customer;
-    })
+    });
 
-    this.loadCustomerReservations(customer_id, reservation_part_id);
+    this.loadCustomerReservations(customerId, reservationPartId);
 
-    this.$mdSidenav('right').open()
+    this.$mdSidenav('right').open();
   }
 
-  loadCustomerReservations(customer_id, reservation_part_id) {
-    this.Customer.searchReservationsByCustomerId(this.current_company_id, customer_id).then(reservations => {
-      this.$rootScope.customer_reservations = reservations;
+  loadCustomerReservations(customerId, reservationPartId) {
+    this.Customer.searchReservationsByCustomerId(this.current_company_id, customerId)
+      .then((reservations) => {
+        this.$rootScope.customer_reservations = reservations;
 
-      if (!reservation_part_id && reservations.count > 0 && reservations[0].reservation_parts.count > 0)
-        reservation_part_id = reservations[0].reservation_parts[0].id;
+        if (!reservationPartId && reservations.count > 0 &&
+          reservations[0].reservation_parts.count > 0) {
+          reservationPartId = reservations[0].reservation_parts[0].id;
+        }
 
-      reservations.forEach((reservation) => {
-        reservation.reservation_parts.forEach((part) => {
-          if (part.id == reservation_part_id)
-            this.$rootScope.current_reservation_part = part;
+        reservations.forEach((reservation) => {
+          reservation.reservation_parts.forEach((part) => {
+            if (part.id === reservationPartId) {
+              this.$rootScope.current_reservation_part = part;
+            }
+          });
         });
       });
-    })
   }
 
   closeCustomerMenu() {
-    this.$mdSidenav('right').close()
+    this.$mdSidenav('right').close();
   }
 
   parsedDate(date) {
