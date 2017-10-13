912ff8b72571a93913c30bc5a43bc845e7195f2e
Oleg Lukashev <incognitorus@gmail.com>
2017-10-13 23:34:47 +0400

    fix change request status for dashboard reservation

--- app/shared/reservation_status_menu/reservation_status_menu.factory.js
+++ app/shared/reservation_status_menu/reservation_status_menu.factory.js
@@ -5,23 +5,16 @@ export default function reservationStatusMenu(ReservationStatus, filterFilter, m
     that.changeStatus = (currentReservation, status) => {
       ReservationStatus
         .changeStatus(that.current_company_id, currentReservation, status).then((reservation) => {
-          if (that.action_required) {
-            const datetime = reservation.reservation_parts[0];
-            const actionRequiredHasReservation =
-              filterFilter(that.action_required, { id: reservation.id })[0];
+          if (typeof that.action_required !== 'undefined') {
+            const reservations = ReservationStatus
+              .translateAndcheckStatusForDelay(that.all_reservations.action_required);
+            const actionRequiredReservation = filterFilter(reservations, { id: reservation.id })[0];
 
-            if (reservation.status === 'request' &&
-                datetime &&
-                moment(datetime) >= that.moment()) {
-              if (!actionRequiredHasReservation) {
-                that.action_required.push(reservation);
-              }
+            if (reservation.status === 'request') {
+              if (!actionRequiredReservation) reservations.push(reservation);
             } else {
               const index = that.action_required.map(item => item.id).indexOf(reservation.id);
-
-              if (actionRequiredHasReservation) {
-                that.action_required.splice(index, 1);
-              }
+              if (actionRequiredReservation) reservations.splice(index, 1);
             }
           }
 
