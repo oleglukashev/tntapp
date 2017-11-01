3fb2aeb683e960769a9f10bf1b2e01dc46b122b5
Oleg Lukashev <incognitorus@gmail.com>
2017-11-01 18:01:53 +0400

    add default mail to cancel reservation form

--- app/components/reservation_answer/reservation_answer.controller.js
+++ app/components/reservation_answer/reservation_answer.controller.js
@@ -1,17 +1,21 @@
 export default class ReservationsAnswerCtrl {
-  constructor(
-    User, ReservationStatus, reservation, $modalInstance,
-    $rootScope, $window, isCancellingReservation,
-  ) {
+  constructor(User, ReservationStatus, reservation, $modalInstance, Settings,
+    filterFilter, $rootScope, $window, isCancellingReservation) {
     'ngInject';
 
     this.current_company_id = User.getCompanyId();
+    this.User = User;
     this.$modalInstance = $modalInstance;
     this.reservation = reservation;
     this.isCancellingReservation = isCancellingReservation;
     this.$rootScope = $rootScope;
     this.$window = $window;
     this.ReservationStatus = ReservationStatus;
+    this.Settings = Settings;
+    this.filterFilter = filterFilter;
+    this.form_data = {};
+
+    this.loadMailsTextsSettings();
   }
 
   closeModal() {
@@ -46,5 +50,29 @@ export default class ReservationsAnswerCtrl {
           this.closeModal();
         }
       }, () => {});
+  }
+
+  loadMailsTextsSettings() {
+    this.Settings.getMailsTextsSettings(this.current_company_id)
+      .then(
+        (mailsSettings) => {
+          this.mails_texts_settings = mailsSettings;
+          const currentMail = this.filterFilter(mailsSettings, { type: 'reservering_ontvangen', language: 'NL' })[0];
+
+          if (currentMail) {
+            const fullName = `${this.reservation.customer.first_name} ${this.reservation.customer.last_name}`;
+            let content = currentMail
+              .content
+              .replace('%VOORNAAM%', fullName);
+
+            const company = this.User.getCompany(this.current_company_id);
+            if (company) {
+              content = content.replace('%RESTAURANTNAAM%', company.name);
+            }
+
+            this.form_data.title = currentMail.title;
+            this.form_data.content = content;
+          }
+        });
   }
 }
