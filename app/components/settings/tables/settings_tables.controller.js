8e306219a234fdaa07432024266a18b21be45986
Oleg Lukashev <incognitorus@gmail.com>
2017-11-22 16:03:19 +0400

    implement string to table number

--- app/components/settings/tables/settings_tables.controller.js
+++ app/components/settings/tables/settings_tables.controller.js
@@ -6,68 +6,66 @@ export default class SettingsTablesCtrl {
 
     this.current_company_id = User.getCompanyId();
 
-    this.Table          = Table;
-    this.Zone           = Zone;
-    this.filterFilter   = filterFilter;
-    this.$modal         = $modal;
-    this.$window        = $window;
-    this.is_loaded      = false;
+    this.Table = Table;
+    this.Zone = Zone;
+    this.filterFilter = filterFilter;
+    this.$modal = $modal;
+    this.$window = $window;
+    this.is_loaded = false;
     this.tables_by_zone = {};
-    this.errors         = [];
-    this.opened         = [true];
+    this.errors = [];
+    this.opened = [true];
 
     this.loadZonesAndTables();
   }
 
   submitForm() {
     this.errors = [];
-    let data    = this.getScopeTables().map((item, index) => {
+    const data = this.getScopeTables().map((item, index) => {
       return {
-        table_number      : item.table_number,
-        number_of_persons : item.number_of_persons,
-        position          : index,
-        zones             : item.zones
-      }
+        table_number: item.table_number,
+        number_of_persons: item.number_of_persons,
+        position: index,
+        zones: item.zones,
+      };
     });
 
-    this.Table
-      .save(this.current_company_id, { tables: data })
-        .then((tables) => {
-        },
+    this.Table.save(this.current_company_id, { tables: data })
+      .then(
+        () => {},
         (error) => {
           this.errors = error.data.errors;
         });
   }
 
   loadTables() {
-    this.Table
-      .getAll(this.current_company_id)
-        .then(
-          (tables) => {
-            this.is_loaded      = true;
+    this.Table.getAll(this.current_company_id)
+      .then(
+        (tables) => {
+          this.is_loaded = true;
 
-            angular.forEach(this.zones, (zone) => {
-              this.tables_by_zone[zone.id] = this.filterFilter(tables, { zones: zone.id }).map((item) => {
-                return {
-                  id                : item.id,
-                  table_number      : parseInt(item.table_number),
-                  number_of_persons : parseInt(item.number_of_persons),
-                  position          : parseInt(item.position),
-                  zones             : item.zones
-                }
-              }).sort((a, b) => {
-                let comparison = 0;
+          angular.forEach(this.zones, (zone) => {
+            this.tables_by_zone[zone.id] = this.filterFilter(tables, { zones: zone.id }).map((item) => {
+              return {
+                id: item.id,
+                table_number: item.table_number,
+                number_of_persons: parseInt(item.number_of_persons),
+                position: parseInt(item.position),
+                zones: item.zones,
+              };
+            }).sort((a, b) => {
+              let comparison = 0;
 
-                if (a.position > b.position) {
-                  comparison = 1;
-                } else if (b.position > a.position) {
-                  comparison = -1;
-                }
+              if (a.position > b.position) {
+                comparison = 1;
+              } else if (b.position > a.position) {
+                comparison = -1;
+              }
 
-                return comparison;
-              });
+              return comparison;
             });
           });
+        });
   }
 
   loadZonesAndTables() {
@@ -77,92 +75,89 @@ export default class SettingsTablesCtrl {
           this.zones = result;
           this.loadTables();
         },
-        (error) => {
+        () => {
         });
   }
 
   addZone() {
-    let that = this;
-    let modalInstance = this.$modal.open({
+    const that = this;
+    const modalInstance = this.$modal.open({
       templateUrl: 'settings_tables.new_zone.view.html',
       controller: 'SettingsTablesNewZoneCtrl as new_zone',
       size: 'md',
       resolve: {
-          zones: function () {
-            return that.zones;
-          }
-        }
+        zones: () => that.zones,
+      },
     });
 
-    modalInstance.result.then((selectedItem) => {
-      //success
-    }, () => {
-      // fail
-    });
+    modalInstance.result.then(() => {}, () => {});
   }
 
   removeZone(index) {
-    let zone = this.zones[index];
+    const zone = this.zones[index];
 
     if (zone) {
-      this.Zone
-        .delete(this.current_company_id, zone.id)
-          .then(
-            (result) => {
-              delete this.tables_by_zone[zone.id];
-              this.zones.splice(index, 1);
-            },
-            (error) => {
-            });
+      this.Zone.delete(this.current_company_id, zone.id)
+        .then(
+          () => {
+            delete this.tables_by_zone[zone.id];
+            this.zones.splice(index, 1);
+          },
+          () => {});
     }
   }
 
   addTableByZoneId(id) {
-    let scope_tables       = this.getScopeTables();
-    let last_position      = scope_tables.length ? Math.max.apply(Math, scope_tables.map((item) => item.position)) : null;
-    let last_table_number  = scope_tables.length ? Math.max.apply(Math, scope_tables.map((item) => item.table_number)) : null;
+    const scopeTables = this.getScopeTables();
+    const lastPosition = scopeTables.length ?
+      Math.max.apply(Math, scopeTables.map(item => item.position)) :
+      null;
 
-    let position           = last_position ? last_position + 1 : 0;
-    let table_number       = last_table_number ? last_table_number + 1 : 1;
+    const lastTableNumber = scopeTables.length ?
+      Math.max.apply(Math, scopeTables.map(item => item.table_number)) :
+      null;
+
+    const position = lastPosition ? lastPosition + 1 : 0;
+    const tableNumber = lastTableNumber || String(1);
 
     if (!this.tables_by_zone[id]) {
       this.tables_by_zone[id] = [];
     }
 
     this.tables_by_zone[id].push({
-      table_number      : table_number,
-      number_of_persons : 0,
-      position          : position,
-      zones             : [id]
+      table_number: tableNumber,
+      number_of_persons: 0,
+      position,
+      zones: [id],
     });
 
     this.submitForm();
   }
 
-  removeTable(zone_id, index) {
-    let table = this.tables_by_zone[zone_id][index];
+  removeTable(zoneId, index) {
+    const table = this.tables_by_zone[zoneId][index];
 
     if (table) {
-      this.tables_by_zone[zone_id].splice(index, 1);
+      this.tables_by_zone[zoneId].splice(index, 1);
       this.submitForm();
     }
   }
 
-  getTableNumberNameByZoneAndIndex(zone_id, index) {
-    return 'table_number_' + zone_id + '_' + index;
+  getTableNumberNameByZoneAndIndex(zoneId, index) {
+    return `table_number_${zoneId}_${index}`;
   }
 
   getGlobalPosition(table) {
-    //need to find a normal alternative for angular forEach
-    let index      = -1;
-    let keep_going = true;
+    // need to find a normal alternative for angular forEach
+    let index = -1;
+    let keepGoing = true;
 
-    angular.forEach(this.getScopeTables().sort((a,b) => a.position - b.position), (item) => {
-      if (keep_going) {
-        index++;
+    angular.forEach(this.getScopeTables().sort((a, b) => a.position - b.position), (item) => {
+      if (keepGoing) {
+        index += 1;
 
         if (JSON.stringify(item) === JSON.stringify(table)) {
-          keep_going = false;
+          keepGoing = false;
         }
       }
     });
