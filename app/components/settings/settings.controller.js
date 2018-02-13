63a68da2332e618b8cd53ed50815707262854f42
Oleg Lukashev <incognitorus@gmail.com>
2018-02-13 18:39:57 +0400

    #333: add prevent for settings routes

--- app/components/settings/settings.controller.js
+++ app/components/settings/settings.controller.js
@@ -1,10 +1,14 @@
 export default class SettingsCtrl {
-  constructor($state, $stateParams, $window) {
+  constructor($state, $stateParams, $window, User) {
     'ngInject';
 
     this.$state = $state;
     this.$stateParams = $stateParams;
     this.$window = $window;
+
+    if (this.$state.current.name.indexOf('app.settings') >= 0 && !User.isOwner()) {
+      this.$state.go('app.dashboard');
+    }
 
     this.pages = {
       'ALGEMEEN': 'app.settings.general',
