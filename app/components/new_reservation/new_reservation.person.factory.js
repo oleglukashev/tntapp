e8679a9b8887339816adad1d73a257b0c95eb138
Oleg Lukashev <incognitorus@gmail.com>
2018-02-14 20:44:20 +0400

    #340: add download pdf for new reservation form

--- app/components/new_reservation/new_reservation.person.factory.js
+++ app/components/new_reservation/new_reservation.person.factory.js
@@ -1,3 +1,5 @@
+import angular from 'angular';
+
 export default function NewReservationPersonFactory() {
   'ngInject';
 
@@ -10,12 +12,27 @@ export default function NewReservationPersonFactory() {
       instance.opened = true;
     };
 
-    instance.removePdf = () => {
+    instance.removePdf = ($event) => {
+      $event.stopPropagation();
       instance.reservation.reservation_pdf = null;
     };
 
     instance.triggerAdditionalInfo = () => {
       instance.additional_is_opened = !instance.additional_is_opened;
+    };
+
+    instance.loadPDF = () => {
+      const file = instance.reservation.reservation_pdf;
+
+      if (typeof file !== 'object') return false;
+
+      const url = (window.URL || window.webkitURL).createObjectURL(file);
+      const anchor = angular.element('<a/>');
+      anchor.attr({
+        href: url,
+        target: '_blank',
+        download: `${file.name}`,
+      })[0].click();
     };
 
     instance.showAutocompleteCustomerName = () =>
